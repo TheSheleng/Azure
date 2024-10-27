17298ab02f92a70d141407b7523a1a7b7fb09c54
TheSheleng <128699288+TheSheleng@users.noreply.github.com>
2024-10-27 11:33:41 +0200

    blob

---
+++ AzureBlob/Program.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Storage.Blobs;
+
+using Microsoft.Extensions.Configuration;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlobAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = Shared.Configuration.Build().GetConnectionString("Storage");
+            BlobServiceClient serviceClient = new BlobServiceClient(connectionString);
+            BlobContainerClient containerClient = serviceClient.GetBlobContainerClient("container1");
+
+            foreach (var blob in containerClient.GetBlobs())
+            {
+                var reference = containerClient.GetBlobClient(blob.Name);
+                reference.DownloadTo(blob.Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
